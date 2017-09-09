e7b6f5448860cb912aa7a0b082d47df57c376514
LaelRodrigues <laelrodrigues7@gmail.com>
2017-09-09 19:35:09 -0300

    Implementação da classe Empresa

---
+++ include/questao1/empresa.h
@@ -0,0 +1,73 @@
+/**
+ * @file	empresa.h
+ * @brief	Arquivo cabecalho com a definicao da classe Empresa
+ 					que representa um empresa
+ * @author	Lael Rodrigues (laelrodrigues7@gmail.com)
+ * @since	06/05/2017
+ * @date	07/05/2017
+ */
+
+#ifndef EMPRESA_H
+#define EMPRESA_H
+
+#include "funcionario.h"
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+
+/**
+ * @class   Empresa empresa.h
+ * @brief   Classe que representa uma empresa
+ * @details Os atributos de um empresa sao seu nome, cnpj e funcionarios
+ */
+
+class Empresa {
+	private:
+		string nome;				/**< Nome da empresa */
+		string cnpj;				/**< Cnpj da empresa(formato: XX.XXX.XXX/YYYY-ZZ) */
+		vector<Funcionario*> funcionarios;		/** < Lista de funcionarios */
+
+	public: 
+		/** @brief Atributo estatico */
+		static int numEmpresas; 
+
+		/** @brief Construtor padrao */
+		Empresa();
+
+		/** @brief Destrutor padrao */
+		~Empresa();									
+
+		/** @brief Construtor parametrizado */
+		Empresa(string n, string c, Funcionario &f);
+
+		/** @brief Retorna o nome da empresa */
+		string getNome();								
+
+		/** @brief Modificar o nome da empresa */
+		void setNome(string n);							
+
+		/** @brief Retorna o cnpj da empresa */
+		string getCnpj();								
+
+		/** @brief Modifica o cnpj da empresa */
+		void setCnpj(string c);
+
+		/** @brief Retorna os funcionarios da empresa */
+		vector<Funcionario*> getFuncionarios();
+
+		/** @brief Modifica o funcionarios da empresa */
+		void setFuncionarios(Funcionario &f);								
+
+		/** @brief Sobrecarga do operador de extracao de stream */
+		friend istream& operator>> (istream& entrada, Funcionario& f);
+
+		/** @brief Sobrecarga do operador de insercao de stream */
+		friend ostream& operator<< (ostream& entrada, Funcionario& f);
+
+};
+
+#endif
---
+++ src/questao1/empresa.cpp
@@ -0,0 +1,95 @@
+/**
+ * @file	empresa.cpp
+ * @brief	Arquivo corpo com a implementacao dos metodos da 
+ 						classe Empresa
+ * @author	Lael Rodrigues (laelrodrigues7@gmail.com)
+ * @since	05/09/2017
+ * @date	09/09/2017
+ */ 
+
+#include "empresa.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <istream>
+using std::istream;
+
+#include <ostream>
+using std::ostream;
+
+Empresa::Empresa(){
+	cnpj = " ";
+	numEmpresas++;
+	// Construtor padrao
+}
+
+/**
+ * @param n nome
+ * @param s cnpj
+ * @param f funcionario
+ */
+
+Empresa::Empresa(string n, string c, Funcionario &f) {
+	setNome(n);
+	setCnpj(cnpj);
+	setFuncionarios(f);
+	numEmpresas++;
+
+}
+
+/** @return Nome */
+string Empresa::getNome(){
+	return nome;
+}
+
+/** @param n Nome */
+void Empresa::setNome(string n){
+	nome = n;
+}
+
+/** @return cnpj */
+string Empresa::getCnpj() {
+	return cnpj;
+}
+
+/**	
+ * @details Se o valor dado para o CNPJ for invalido o programa
+ *			exibe uma mesagem de erro e termina a execucao.
+ * @param c cnpj
+ */
+void Empresa::setCnpj(string c) {
+	if((int)c.size() != 14 ) {
+		cout << "Numero de CNPJ invalido!!" << endl;
+		cout << "Modo de uso: Digite 14 digitos." << endl;
+		return;
+	}
+	for(int i = 0; i < (int)c.size(); i++) {
+		if(c[i] < 48|| c[i] > 57) {
+			cout << "Erro: Numero de CNPJ invalido!!" << endl;
+			cout << "Modo de uso: Digite 14 digitos(valores inteiros)." << endl;
+			return;
+		}
+	}
+	cnpj = c;
+}
+
+/** @return	apontador para a lista de funcionairos */
+vector<Funcionario*> Empresa::getFuncionarios() {
+	return funcionarios;
+}
+
+/** @param f referencia para um objeto funcionario */
+void Empresa::setFuncionarios(Funcionario &f) {
+	Funcionario::numFuncionarios++;
+	funcionarios.push_back(new Funcionario(f));
+}
+
+//Destrutor padrao
+Empresa::~Empresa() {
+	funcionarios.clear();
+}
